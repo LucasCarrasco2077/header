4d272b1f74787a1a5217748c46f1404c91410c62
Lucas Carrasco <72756808+LucasCarrasco2077@users.noreply.github.com>
2022-03-28 10:47:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const bar = document.querySelector('.bar-area')
+const menu = document.querySelector('.lateral')
+
+bar.onclick = () => {
+  
+        menu.style.width = '150px'
+        menu.style.color = 'black'
+    
+}
+
